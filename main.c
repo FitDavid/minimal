fcb5ba986685f81bef958c4d11c62c182740c913
David Fitala <davidfitala@gmail.com>
2022-11-01 21:30:22 +0100

    link .data and .bss correctly

--- main.c
+++ main.c
@@ -2,57 +2,97 @@
 
 uint32_t *vtable[100] __attribute__((section(".isr_vector"))) =
 {
-		[0] = (uint32_t *)SRAM_END, // MSP
-		[1] = (uint32_t *)main,
-		[11] = (uint32_t *)SVC_Handler,
-		[15] = (uint32_t *)SysTick_Handler
+	[0] = (uint32_t *)SRAM_END, // MSP
+	[1] = (uint32_t *)main,
+	[11] = (uint32_t *)SVC_Handler,
+	[15] = (uint32_t *)SysTick_Handler
 };
 
 uint8_t minsp1[STACK_SIZE] __attribute__ ((aligned(8))); // pointer to minimal sp
 uint8_t minsp2[STACK_SIZE] __attribute__ ((aligned(8))); // aligned to 8 byte boundary
 
 struct threadtbl_t threads;
+uint32_t vardata = 0xA5;
+uint32_t varbss;
 
 int main()
 {
-		struct stackframe_t * sp1 = (struct stackframe_t*)(minsp1 + STACK_SIZE - sizeof(struct stackframe_t));
-		struct stackframe_t  * sp2 = (struct stackframe_t*)(minsp2 + STACK_SIZE - sizeof(struct stackframe_t));
-		sp1->retaddr = (uint32_t*)ledon; //
-		sp1->xpsr = 1ul << 24ul;
-		sp2->retaddr = (uint32_t*)ledoff; //
-		sp2->xpsr = 1ul << 24ul;
-		threads.tbl[0].sp = (uint32_t*) sp1;
-		threads.tbl[1].sp = (uint32_t*) sp2;
-		*RCC_AHB1ENR = 0x1;
-		*GPIOA_MODER |= 0x400;
-		asm("svc 0 \n\t");
-		while(1)
-		{
-				*GPIOA_ODR = 0x20;
-				delay(2000000);
-				*GPIOA_ODR = 0x0;
-				delay(2000000);
-		}
+	struct stackframe_t * sp1 = (struct stackframe_t*)(minsp1 + STACK_SIZE - sizeof(struct stackframe_t));
+	struct stackframe_t  * sp2 = (struct stackframe_t*)(minsp2 + STACK_SIZE - sizeof(struct stackframe_t));
+	uint32_t time;
+	sp1->retaddr = (uint32_t*)ledon; //
+	sp1->xpsr = 1ul << 24ul;
+	sp2->retaddr = (uint32_t*)ledoff; //
+	sp2->xpsr = 1ul << 24ul;
+	threads.tbl[0].sp = (uint32_t*) sp1;
+	threads.tbl[1].sp = (uint32_t*) sp2;
+	*RCC_AHB1ENR = 0x1;
+	*GPIOA_MODER |= 0x400;
+	if(varbss == 0)
+	{
+		time = 0xFFFFF;
+	}
+	else
+	{
+		time = 0xFFFFFF;
+	}
+	//asm("svc 0 \n\t");
+	while(1)
+	{
+		*GPIOA_ODR = 0x20;
+		delay(time);
+		*GPIOA_ODR = 0x0;
+		delay(time);
+	}
 }
 
 void ledon()
 {
-		while(1)
-		{
-				//*GPIOA_ODR = 0x20;
-		}
+	while(1)
+	{
+		//*GPIOA_ODR = 0x20;
+	}
 }
 
 void ledoff()
 {
-		while(1)
-		{
-				*GPIOA_ODR = 0x0;
-		}
+	while(1)
+	{
+		*GPIOA_ODR = 0x0;
+	}
 }
 
 void delay(volatile uint32_t count)
 {
-		while(count--);
+	while(count--);
 }
 
+/*-----------Init routine------------*/
+
+/*----linker defined variables----*/
+extern uint32_t _sidata, _sdata, _edata;
+extern uint32_t _sbss, _ebss;
+
+
+void _init_data(uint32_t* sidata, uint32_t* sdata, uint32_t* edata)
+{
+	while(sdata < edata)
+	{
+		*sdata++ = *sidata++;
+	}
+}
+
+void _init_bss(uint32_t* sbss, uint32_t* ebss)
+{	
+	while(sbss < ebss)
+	{
+		*sbss = 0;
+	}
+}
+
+void _start()
+{
+	_init_data(&_sidata, &_sdata, &_edata);
+	_init_bss(&_sbss, &_ebss);
+	main();
+}
